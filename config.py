9803a97c4830b3c6c2b572d62a3e514552cdb58f
fanyilun <fanyilun0@gmail.com>
2024-11-27 16:24:11 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+WEBHOOK_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=
+PROXY_URL = 'http://localhost:7890'
+USE_PROXY = True
+ALWAYS_NOTIFY = True
+
+# HTML URL
+API_URL = "https://www.binance.com/en/support/announcement/new-cryptocurrency-listing?c=48&navId=48&hl=en"
+
+# 监控周期，单位：秒
+MONITOR_INTERVAL = 60  # 每隔 60 秒查询一次
