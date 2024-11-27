4958a6ca9588bb13bd83a9ca490aa448cf2146d9
fanyilun <fanyilun0@gmail.com>
2024-11-27 16:27:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 WEBHOOK_URL = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key='
 PROXY_URL = 'http://localhost:7890'
-USE_PROXY = True
-ALWAYS_NOTIFY = True
+USE_PROXY = False
+ALWAYS_NOTIFY = False
 
 # HTML URL
 API_URL = "https://www.binance.com/en/support/announcement/new-cryptocurrency-listing?c=48&navId=48&hl=en"
